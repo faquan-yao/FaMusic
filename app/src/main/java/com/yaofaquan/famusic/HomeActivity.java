e388f6dcd3d700bfd736339d90f91ec31d09ebab
faquan.yao <faquan.yao@gmail.com>
2021-09-02 18:59:45 +0800

    搭建FaMusic的基本框架

---
+++ app/src/main/java/com/yaofaquan/famusic/HomeActivity.java
@@ -0,0 +1,36 @@
+package com.yaofaquan.famusic;
+
+import android.os.Bundle;
+
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.view.View;
+
+import androidx.fragment.app.FragmentActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+import com.yaofaquan.famusic.databinding.ActivityHomeBinding;
+
+public class HomeActivity extends FragmentActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+        initView();
+        initData();
+    }
+
+    private void initData() {
+
+    }
+
+    private void initView() {
+
+    }
+}
